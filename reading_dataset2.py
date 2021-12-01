7020faf9d84a0a284419d29fec629c872d76d490
amirBgazand <amirghafari44@gmail.com>
2021-12-01 21:05:40 +0330

    complete plot function

--- reading_dataset2.py
+++ reading_dataset2.py
@@ -91,40 +91,116 @@ def read_ecg(index,folder_number=5):
     return ecg
 
     
+def plot_ecg_form(ecg , mat,time, form):
+    dx=ecg.dx
+    new=[label2diagnosis(x) for x in ecg.dx]
+    x=np.arange(ecg.samples, )/ecg.sample_rate
+    y=mat/1000
+    maxy=y.max()
+    miny=y.min()
+    fig, ax = plt.subplots(6,2)
+    if form == 'normal':
+        leads=['I','II','III','aVR','aVL','aVF','V1','V2','V3','V4','V5','V6']
+    else:         
+        y_new=np.empty((12,int(ecg.samples)))
+        y_new[0,:]=y[4,:]
+        y_new[1,:]=y[0,:]
+        y_new[2,:]=-y[3,:]
+        y_new[3,:]=y[1,:]
+        y_new[4,:]=y[5,:]
+        y_new[5,:]=y[2,:]
+        y_new[6:12,:]=y[6:12,:]
+        leads =['aVL','I','-aVR','II','aVF','III','V1','V2','V3','V4','V5','V6']
+        y=y_new
+
+    for i in range (6):
+        for j in range (2):
+            y=mat[i+6*j,:]/1000
+            ax[i,j].plot(x,y)
+            ax[i,j].set_ylabel (leads[i+6*j])
+            
+            tool=ecg.duration
+            small_vertical=np.arange(0,tool ,0.04)  
+            big_vertical=np.arange(0,tool,0.2)
+
+            [ax[i,j].axvline(x=k, linestyle='--',linewidth=0.1 ) for k in small_vertical]
+            [ax[i,j].axvline(x=k, linestyle='--',linewidth=0.5 ) for k in big_vertical]
+
+            small_horizontal=np.arange(-3,+3 ,0.1) 
+            big_horizontal=np.arange(-3,+3,0.5)
+            [ax[i,j].axhline(y=k, linestyle='--',linewidth=0.1 ) for k in small_horizontal]
+            [ax[i,j].axhline(y=k, linestyle='--',linewidth=0.5 ) for k in big_horizontal]
+            ax[i,j].axis([time[0], time[1],miny,maxy])
+    
+    fig.suptitle(f'age:{ecg.age} , dx:{str(new)} ' , fontsize=10 )        
+    plt.subplots_adjust(left=0.05, bottom=0.05, right=0.97, top=0.95, wspace=.12, hspace=0.22)        
 
 
 
-
-def plot_ecg (index,channels=None,folder_number=5):
+def plot_ecg (index,time=None,channels=None,folder_number=5 , form='better'):
     ecg_mat_file=get_ecg(index,folder_number)[0]
     ecg= read_ecg(index , folder_number)
     mat = scipy.io.loadmat(ecg_mat_file)
     mat=mat['val']
-   
-    subplot(channels,ecg,mat)
-    
+    # subplot(channels,ecg,mat)
+    if time != None:
+        assert type(time)==tuple  
+    else:
+        time=(0,ecg.samples/ecg.sample_rate)  
+
+    plot_ecg_form(ecg,mat,time ,form)
+        
     plt.show()
 
 
-def subplot (channels , ecg , mat):
+def subplot (channels , ecg , mat ):
     leads=['I','II','III','aVR','aVL','aVF','V1','V2','V3','V4','V5','V6']
     x=np.arange(ecg.samples, )/ecg.sample_rate
-    if type(channels)!=list:
+    dx=ecg.dx
+    new=[label2diagnosis(x) for x in ecg.dx]
+    
+    if type(channels)!=list and channels!=None :
         plt.plot(x,mat [channels ,:]/1000)
         plt.ylabel(leads[channels])
+       
     else:    
         if channels==None:
             channels=[i for i in range(12)]
-        
+    
         x=np.arange(ecg.samples, )/ecg.sample_rate
         num=len(channels)
         fig, ax = plt.subplots(num)
+
         for i in range (num):
+            
             y=mat[channels[i],:]/1000
+            maxy=y.max()
+            miny=y.min()
+            tool=ecg.duration
+            small_vertical=np.arange(0,tool ,0.04)  
+            big_vertical=np.arange(0,tool,0.2)
+
+            [ax[i].axvline(x=j, linestyle='--',linewidth=0.1 ) for j in small_vertical]
+            [ax[i].axvline(x=j, linestyle='--',linewidth=0.5 ) for j in big_vertical]
+
+            small_horizontal=np.arange(miny,maxy ,0.1) 
+            big_horizontal=np.arange(miny,maxy,0.5)
+            [ax[i].axhline(y=j, linestyle='--',linewidth=0.1 ) for j in small_horizontal]
+            [ax[i].axhline(y=j, linestyle='--',linewidth=0.5 ) for j in big_horizontal]
             ax[i].plot(x,y)
             ax[i].set_ylabel (leads[channels[i]])
+            plt.subplots_adjust(left=0.07, bottom=0.02, right=0.98, top=0.98, wspace=0, hspace=0)
+            
+        fig.suptitle(f'age:{ecg.age} , dx:{str(new)} ' , fontsize=10 )
 
-# plot_ecg (15, folder_number=0 , channels=[4,5] )
+plot_ecg (4,(0,6))
+
+
+
+
+
+
+
 
 
 
@@ -160,9 +236,4 @@ def find_coronaries(folder_number):
                     break
     return coronery_ids   , coroneries
     
-# print(find_normals(5))
-a=find_coronaries(5)
-with open ('file3.txt' , 'w') as file:
-    for item in a :
-        file.write(str(item)+'\n')
 
